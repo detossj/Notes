7c3a2f763e5f3475e46fae04b852f1e2a920402d
detossj <jorgerubilarssj@gmail.com>
2025-05-03 16:17:06 -0400

    functions added

--- app/src/main/java/com/deto/notes/data/NoteRepository.kt
+++ app/src/main/java/com/deto/notes/data/NoteRepository.kt
@@ -1,4 +1,17 @@
 package com.deto.notes.data
 
+import kotlinx.coroutines.flow.Flow
+
 interface NoteRepository {
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun updateNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+    fun getNoteStream(title: String, content: String): Flow<Note?>
+
+    fun getAllNotesStream(): Flow<List<Note>>
+
 }
