21eb336c660d3bdd5470732f39759e054c555930
detossj <jorgerubilarssj@gmail.com>
2025-05-03 16:13:58 -0400

    functions added

--- app/src/main/java/com/deto/notes/data/NoteDao.kt
+++ app/src/main/java/com/deto/notes/data/NoteDao.kt
@@ -1,4 +1,28 @@
 package com.deto.notes.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Note)
+
+    @Update
+    suspend fun update(item: Note)
+
+    @Delete
+    suspend fun delete(item: Note)
+
+    @Query("SELECT * from notes WHERE content = :content OR title = :title")
+    fun getItem(title: String,content: String)
+
+    @Query("SELECT * from notes")
+    fun getAllItems() : Flow<List<Note>>
 }
