8e6f50144b8684939ed39be7cdb2f1c68252a360
detossj <jorgerubilarssj@gmail.com>
2025-05-03 16:06:42 -0400

    data class Note added

--- app/src/main/java/com/deto/notes/data/Note.kt
+++ app/src/main/java/com/deto/notes/data/Note.kt
@@ -1,4 +1,14 @@
 package com.deto.notes.data
 
-class Note {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val content: String,
+    val date: LocalDateTime
+)
