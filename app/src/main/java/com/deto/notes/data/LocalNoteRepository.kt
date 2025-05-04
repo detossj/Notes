eaf43c0490500fbc6189f4c868470c5c8d0930a4
detossj <jorgerubilarssj@gmail.com>
2025-05-04 15:06:19 -0400

    functions added

--- app/src/main/java/com/deto/notes/data/LocalNoteRepository.kt
+++ app/src/main/java/com/deto/notes/data/LocalNoteRepository.kt
@@ -1,4 +1,15 @@
 package com.deto.notes.data
 
-class LocalNoteRepository {
+import kotlinx.coroutines.flow.Flow
+
+class LocalNoteRepository(private val itemDao : NoteDao) : NoteRepository {
+    override suspend fun insertNote(note: Note) = itemDao.insert(note)
+
+    override suspend fun updateNote(note: Note) = itemDao.update(note)
+
+    override suspend fun deleteNote(note: Note) = itemDao.delete(note)
+
+    override fun getNoteStream(title: String, content: String): Flow<Note?> = itemDao.getItem(title,content)
+
+    override fun getAllNotesStream(): Flow<List<Note>> = itemDao.getAllItems()
 }
