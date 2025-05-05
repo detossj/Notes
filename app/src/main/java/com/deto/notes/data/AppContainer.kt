61e8d5d8cebc0901ab6b4ae6bb680a3161ff6f84
detossj <jorgerubilarssj@gmail.com>
2025-05-05 12:56:52 -0400

    interface AppContainer and class AppDataContainer added

--- app/src/main/java/com/deto/notes/data/AppContainer.kt
+++ app/src/main/java/com/deto/notes/data/AppContainer.kt
@@ -1,2 +1,14 @@
 package com.deto.notes.data
 
+import android.content.Context
+
+interface AppContainer {
+    val noteRepository: NoteRepository
+}
+
+class AppDataContainer(private val context: Context) : AppContainer {
+    override val noteRepository: NoteRepository by lazy {
+        LocalNoteRepository(AppDatabase.getDatabase(context).noteDao())
+    }
+
+}
