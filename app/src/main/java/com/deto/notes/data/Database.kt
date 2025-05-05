070a3467ba06edbe69f311f5018dad3109a7c9a2
detossj <jorgerubilarssj@gmail.com>
2025-05-05 12:58:04 -0400

    database created

--- app/src/main/java/com/deto/notes/data/Database.kt
+++ app/src/main/java/com/deto/notes/data/Database.kt
@@ -1,2 +1,24 @@
 package com.deto.notes.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        private var Instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, AppDatabase::class.java, "App_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+
+}
