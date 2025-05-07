6f8331cc361b2ecc177b9fb158c88a5b460454af
detossj <jorgerubilarssj@gmail.com>
2025-05-07 14:12:43 -0400

    object AppViewModelProvider and fun CreationExtras added

--- app/src/main/java/com/deto/notes/ui/ViewModelProvider.kt
+++ app/src/main/java/com/deto/notes/ui/ViewModelProvider.kt
@@ -1,2 +1,15 @@
 package com.deto.notes.ui
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.deto.notes.Notes
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+
+    }
+}
+
+fun CreationExtras.App(): Notes =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Notes)
