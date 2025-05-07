66a5d0f2308f13ef370e2c243f1d423b25d001a9
detossj <jorgerubilarssj@gmail.com>
2025-05-06 21:28:38 -0400

    class Notes added

--- app/src/main/java/com/deto/notes/AppContainer.kt
+++ app/src/main/java/com/deto/notes/AppContainer.kt
@@ -1,2 +1,15 @@
 package com.deto.notes
 
+import android.app.Application
+import com.deto.notes.data.AppContainer
+import com.deto.notes.data.AppDataContainer
+
+class Notes : Application() {
+
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppDataContainer(this)
+    }
+}
