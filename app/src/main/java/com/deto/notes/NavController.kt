c5771b13acc9e5a37fca1c7a02e68f069b6b4c12
detossj <jorgerubilarssj@gmail.com>
2025-05-07 14:23:12 -0400

    Composable fun added

--- app/src/main/java/com/deto/notes/NavController.kt
+++ app/src/main/java/com/deto/notes/NavController.kt
@@ -1,8 +1,30 @@
 package com.deto.notes
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.deto.notes.ui.screens.HomeScreen
+import com.deto.notes.ui.screens.NoteScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+object Note
 
 @Composable
 fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Home) {
+        composable<Home> {
+            HomeScreen(navController)
+        }
+        composable<Note> {
+            NoteScreen(navController)
+        }
+    }
 
 }
--- app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
@@ -1,2 +1,9 @@
 package com.deto.notes.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(Navigation: NavController){
+
+}
