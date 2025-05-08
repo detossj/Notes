478b73d5a6579614911be92623f4b372d4f48092
detossj <jorgerubilarssj@gmail.com>
2025-05-08 14:35:53 -0400

    LargeTopAppBar added

--- app/src/main/java/com/deto/notes/ui/components/CustomTopAppBar.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomTopAppBar.kt
@@ -1,2 +1,34 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import kotlin.math.sin
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTopAppBar(scrollBehavior : TopAppBarScrollBehavior) {
+
+    LargeTopAppBar(
+        title = {
+            Text("Notas") },
+        scrollBehavior = scrollBehavior
+    )
+
+}
