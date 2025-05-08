eb8602729dbe10415eb7657c4a7fdf8a8dab8964
detossj <jorgerubilarssj@gmail.com>
2025-05-08 14:37:23 -0400

    SearchTopAppBar and SearchContent added

--- app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
@@ -1,9 +1,30 @@
 package com.deto.notes.ui.screens
 
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.NavController
+import com.deto.notes.ui.components.SearchContent
+import com.deto.notes.ui.components.SearchTopAppBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(Navigation: NavController){
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val scrollState = rememberLazyListState()
+
+    Scaffold(
+        topBar = {
+            SearchTopAppBar(scrollBehavior = scrollBehavior)
+        },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { innerPadding ->
+        SearchContent(innerPadding = innerPadding, scrollState = scrollState)
+    }
 
 }
