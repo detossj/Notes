1e41f52cf324388eab4910f8c4a2a627c4fcfa68
detossj <jorgerubilarssj@gmail.com>
2025-05-08 14:37:04 -0400

    LazyColumn and OutlinedTextField added

--- app/src/main/java/com/deto/notes/ui/components/SearchContent.kt
+++ app/src/main/java/com/deto/notes/ui/components/SearchContent.kt
@@ -1,2 +1,43 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchContent(innerPadding: PaddingValues, scrollState: LazyListState) {
+    LazyColumn(
+        state = scrollState,
+        contentPadding = innerPadding
+    ) {
+        item {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                label = { Text("Buscar notas") },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = "Buscar notas"
+                    )
+                }
+            )
+        }
+
+
+    }
+}
