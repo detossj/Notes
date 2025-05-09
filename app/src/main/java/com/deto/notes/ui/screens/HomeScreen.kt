ef31049847a4ff184364c272f2db6813c2e76195
detossj <jorgerubilarssj@gmail.com>
2025-05-09 19:47:39 -0400

    noteList and fun added

--- app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.deto.notes.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -7,9 +10,27 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.deto.notes.data.Note
+import com.deto.notes.ui.components.CustomTopAppBar
+import com.deto.notes.ui.components.NoteList
 import com.deto.notes.ui.components.SearchContent
-import com.deto.notes.ui.components.SearchTopAppBar
+
+
+val notesList = listOf(
+    Note(1, "Nota 1", "Contenido de la nota 1"),
+    Note(2, "Nota 2", "Contenido de la nota 2"),
+    Note(3, "Nota 3", "Contenido de la nota 3"),
+    Note(4, "Nota 4", "Contenido de la nota 4"),
+    Note(5, "Nota 5", "Contenido de la nota 5"),
+    Note(6, "Nota 6", "Contenido de la nota 6"),
+    Note(7, "Nota 7", "Contenido de la nota 7"),
+    Note(8, "Nota 8", "Contenido de la nota 8"),
+    Note(9, "Nota 9", "Contenido de la nota 9"),
+    Note(10, "Nota 10", "Contenido de la nota 10")
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,11 +41,15 @@ fun HomeScreen(Navigation: NavController){
 
     Scaffold(
         topBar = {
-            SearchTopAppBar(scrollBehavior = scrollBehavior)
+            CustomTopAppBar(scrollBehavior)
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
-        SearchContent(innerPadding = innerPadding, scrollState = scrollState)
+
+        Column(modifier = Modifier.padding(innerPadding)) {
+            SearchContent(scrollState)
+            NoteList(PaddingValues(0.dp), notesList)
+        }
     }
 
 }
