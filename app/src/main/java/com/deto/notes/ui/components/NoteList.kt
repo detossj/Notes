ed8da7bb281fec11e85a1661f1e6aa4deea06f3d
detossj <jorgerubilarssj@gmail.com>
2025-05-09 19:48:27 -0400

    LazyVerticalGrid added

--- app/src/main/java/com/deto/notes/ui/components/NoteList.kt
+++ app/src/main/java/com/deto/notes/ui/components/NoteList.kt
@@ -1,2 +1,65 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.deto.notes.data.Note
+
+@Composable
+fun NoteList(innerPadding: PaddingValues, notes: List<Note>) {
+
+    var selected by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(20.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(notes) {
+                Card(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .clickable { selected = it.id },
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .padding(horizontal = 20.dp, vertical = 35.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(it.title)
+                        Text(it.content)
+                    }
+                }
+            }
+        }
+    }
+}
