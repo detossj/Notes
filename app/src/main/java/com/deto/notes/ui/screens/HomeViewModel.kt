45c346c5f82d1bd3c85dd84b84d18ee96a4b77fc
detossj <jorgerubilarssj@gmail.com>
2025-05-12 14:42:27 -0400

    class HomeViewModel added

--- app/src/main/java/com/deto/notes/ui/screens/HomeViewModel.kt
+++ app/src/main/java/com/deto/notes/ui/screens/HomeViewModel.kt
@@ -1,2 +1,14 @@
 package com.deto.notes.ui.screens
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deto.notes.data.LocalNoteRepository
+
+import com.deto.notes.data.NoteRepository
+import kotlinx.coroutines.launch
+
+class HomeViewModel (private val noteRepository: NoteRepository) : ViewModel() {
+
+    val noteList = noteRepository.getAllNotesStream()
+
+}
