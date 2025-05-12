4d519f43e778243be095390cb5bcf84f035d1a22
detossj <jorgerubilarssj@gmail.com>
2025-05-12 14:41:56 -0400

    HomeViewModel added

--- app/src/main/java/com/deto/notes/ui/ViewModelProvider.kt
+++ app/src/main/java/com/deto/notes/ui/ViewModelProvider.kt
@@ -2,12 +2,16 @@ package com.deto.notes.ui
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.deto.notes.Notes
+import com.deto.notes.ui.screens.HomeViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-
+        initializer {
+            HomeViewModel(App().container.noteRepository)
+        }
     }
 }
 
