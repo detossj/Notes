d728169d15ee861db1c9a13926fbffdfeed09daf
detossj <jorgerubilarssj@gmail.com>
2025-05-15 17:49:07 -0400

    homeViewModel and noteId added

--- app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,15 +26,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.deto.notes.data.Note
 import com.deto.notes.ui.AppViewModelProvider
 import com.deto.notes.ui.components.CustomOutlinedTextField
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewNoteScreen(Navigation: NavController, viewModel: NewNoteViewModel = viewModel(factory = AppViewModelProvider.Factory)){
+fun NewNoteScreen(Navigation: NavController, noteId: Int, viewModel: NewNoteViewModel = viewModel(factory = AppViewModelProvider.Factory)){
+
+    val homeViewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+
+    val noteList by homeViewModel.noteList.collectAsState(initial = emptyList())
 
     val scope = rememberCoroutineScope()
+
+    val note = noteList.find { it.id == noteId }
 
     //  Manejo del botón "Atras" del sistema
     BackHandler {
@@ -81,9 +89,16 @@ fun NewNoteScreen(Navigation: NavController, viewModel: NewNoteViewModel = viewM
                 modifier = Modifier.fillMaxSize().padding(16.dp)
             ) {
 
-                CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },"Título",32)
+               if(note == null) {
+                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },"Título",32)
 
-                CustomOutlinedTextField(viewModel.newNoteUiState.newNote.content, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(content = it)) },"Empiece a escribir",16)
+                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.content, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(content = it)) },"Empiece a escribir",16)
+               }
+               else {
+                   CustomOutlinedTextField(note.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },note.title,32)
+
+                   CustomOutlinedTextField(note.content, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(content = it)) },note.content,16)
+               }
 
             }
 
