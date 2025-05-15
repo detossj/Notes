4f80e6d36aecddaa304c87d4ac15aa84d5f51b23
detossj <jorgerubilarssj@gmail.com>
2025-05-14 20:12:25 -0400

    renamed

--- app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
@@ -3,9 +3,7 @@ package com.deto.notes.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -13,11 +11,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,15 +20,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.deto.notes.ui.components.CustomOutlinedTextField
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NoteScreen(Navigation: NavController){
+fun NewNoteScreen(Navigation: NavController){
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
--- app/src/main/java/com/deto/notes/ui/screens/NoteViewModel.kt
+++
@@ -1,2 +0,0 @@
-package com.deto.notes.ui.screens
-
