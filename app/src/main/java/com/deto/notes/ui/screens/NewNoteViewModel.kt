e2f90b85b35889ef79350a9ef5781b57f2904e2b
detossj <jorgerubilarssj@gmail.com>
2025-05-14 20:38:19 -0400

    class NewNoteViewModel added

---
+++ app/src/main/java/com/deto/notes/ui/screens/NewNoteViewModel.kt
@@ -0,0 +1,62 @@
+package com.deto.notes.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.deto.notes.data.Note
+import com.deto.notes.data.NoteRepository
+
+class NewNoteViewModel(private val noteRepository: NoteRepository) : ViewModel() {
+
+    var newNoteUiState by mutableStateOf(NewNoteUIState())
+        private set
+
+    fun updateUiState(newNote: NewNote){
+        newNoteUiState =
+            NewNoteUIState(newNote = newNote, isEntryValid = validateInput(newNote))
+    }
+
+    suspend fun saveItem() {
+        if(validateInput()){
+            noteRepository.insertNote(newNoteUiState.newNote.toElement())
+        }
+    }
+
+    private fun validateInput(uiState: NewNote = newNoteUiState.newNote) : Boolean {
+        return with(uiState) {
+            title.isNotBlank() || content.isNotBlank()
+        }
+    }
+
+}
+data class NewNoteUIState(
+    val newNote: NewNote = NewNote(),
+    val isEntryValid: Boolean = false
+)
+
+data class NewNote(
+    val id: Int = 0,
+    val title: String = "",
+    val content: String = "",
+    //val date: LocalDateTime
+)
+
+fun NewNote.toElement(): Note = Note(
+    id = id,
+    title = title,
+    content = content,
+    //val date: LocalDateTime
+)
+
+fun Note.toItemUiState(isEntryValid: Boolean = false): NewNoteUIState = NewNoteUIState(
+    newNote = this.toItemDetails(),
+    isEntryValid = isEntryValid
+)
+
+fun Note.toItemDetails(): NewNote = NewNote(
+    id = id,
+    title = title,
+    content = content,
+    //val date: LocalDateTime
+)
