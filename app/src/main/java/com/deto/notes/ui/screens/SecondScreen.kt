0d0e315378f6430ee223a00b3adcc4695bb53bed
detossj <jorgerubilarssj@gmail.com>
2025-05-20 14:25:59 -0400

    fun SecondScreen created

---
+++ app/src/main/java/com/deto/notes/ui/screens/SecondScreen.kt
@@ -0,0 +1,42 @@
+package com.deto.notes.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.navigation.NavController
+import com.deto.notes.ui.components.CustomBottomAppBar
+import com.deto.notes.ui.components.CustomFloatingActionButton
+import com.deto.notes.ui.components.CustomTopAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SecondScreen(Navigation: NavController) {
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(scrollBehavior,"Tareas")
+        },
+        bottomBar = {
+            CustomBottomAppBar(Navigation)
+        },
+        floatingActionButton = {
+            CustomFloatingActionButton(Navigation)
+        },
+        modifier = Modifier.nestedScroll((scrollBehavior.nestedScrollConnection))
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+
+    }
+}
