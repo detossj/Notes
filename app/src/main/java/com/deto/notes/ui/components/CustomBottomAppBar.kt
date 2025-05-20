923d0b640d3cbd0dcde199ece7bdc9f00a17a9af
detossj <jorgerubilarssj@gmail.com>
2025-05-19 22:58:04 -0400

    fun CustomBottomAppBar() added

--- app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBar.kt
@@ -1,2 +1,79 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import com.deto.notes.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomBottomAppBar() {
+
+    BottomAppBar(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth()
+               ,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painterResource(R.drawable.article_24px),
+                        contentDescription = "Notas",
+                        modifier = Modifier.size(30.dp)
+                    )
+
+                    Text("Notas")
+                }
+            }
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painterResource(R.drawable.assignment_turned_in_24px),
+                        contentDescription = "Tareas",
+                        modifier = Modifier.size(30.dp)
+                    )
+
+                    Text("Tareas")
+                }
+            }
+        }
+    }
+
+}
