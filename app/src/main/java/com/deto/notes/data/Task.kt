567d41d5f06d8cef7b1a146108b42e1f2b033ee5
detossj <jorgerubilarssj@gmail.com>
2025-05-20 14:33:51 -0400

    entity Task added

---
+++ app/src/main/java/com/deto/notes/data/Task.kt
@@ -0,0 +1,13 @@
+package com.deto.notes.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val completed: Boolean
+
+)
