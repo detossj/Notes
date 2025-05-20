94d3fcb7717a013383fcdd1aa9e4bd6a8cb38eb3
detossj <jorgerubilarssj@gmail.com>
2025-05-20 14:48:59 -0400

    interface TaskDao added

--- app/src/main/java/com/deto/notes/data/TaskDao.kt
+++ app/src/main/java/com/deto/notes/data/TaskDao.kt
@@ -1,4 +1,25 @@
 package com.deto.notes.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Task)
+
+    @Update
+    suspend fun update(item: Task)
+
+    @Delete
+    suspend fun delete(item: Task)
+
+    @Query("SELECT * from tasks")
+    fun getAllItems(): Flow<List<Task>>
 }
