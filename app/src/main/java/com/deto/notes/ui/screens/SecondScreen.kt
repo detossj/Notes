9550562a34b6b93028f434e244f382d6198d5f82
detossj <jorgerubilarssj@gmail.com>
2025-05-22 14:07:17 -0400

    TaskList added

--- app/src/main/java/com/deto/notes/ui/screens/SecondScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/SecondScreen.kt
@@ -1,6 +1,7 @@
 package com.deto.notes.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -8,10 +9,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.deto.notes.ui.components.CustomBottomAppBar
 import com.deto.notes.ui.components.CustomFloatingActionButton
 import com.deto.notes.ui.components.CustomTopAppBar
+import com.deto.notes.ui.components.TaskList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,7 +38,7 @@ fun SecondScreen(Navigation: NavController) {
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
-
+            TaskList(PaddingValues(0.dp))
         }
 
     }
