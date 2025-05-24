dabb4fd4a7516235019ef058af566b095c519770
detossj <jorgerubilarssj@gmail.com>
2025-05-23 20:54:54 -0400

    fun CustomFloatingActionButtonSecond added

--- app/src/main/java/com/deto/notes/ui/components/CustomFloatingActionButtonSecond.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomFloatingActionButtonSecond.kt
@@ -1,2 +1,21 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import com.deto.notes.NotePage
+
+@Composable
+fun CustomFloatingActionButtonSecond(onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onClick() },
+        containerColor = Color.Yellow,
+        contentColor = Color.White
+    ) {
+        Icon(Icons.Default.Add, contentDescription = "Agregar Tarea")
+    }
+}
