3e5097898042d18f2cc59b02eb4648c5aa3729d4
detossj <jorgerubilarssj@gmail.com>
2025-05-23 21:18:17 -0400

    bottomSheetState, scope, showBottomSheet and CustomModalBottomSheet added

--- app/src/main/java/com/deto/notes/ui/screens/SecondScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/SecondScreen.kt
@@ -6,40 +6,63 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.deto.notes.data.Task
 import com.deto.notes.ui.components.CustomBottomAppBar
-import com.deto.notes.ui.components.CustomFloatingActionButton
+import com.deto.notes.ui.components.CustomFloatingActionButtonSecond
+import com.deto.notes.ui.components.CustomModalBottomSheet
 import com.deto.notes.ui.components.CustomTopAppBar
 import com.deto.notes.ui.components.TaskList
+import kotlinx.coroutines.launch
+
+var taskList = mutableListOf<Task>(
+    Task(1,"Sacar la ropa",true),
+    Task(2,"Ir a comprar pan",true),
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SecondScreen(Navigation: NavController) {
 
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    CustomModalBottomSheet(showBottomSheet, scope, bottomSheetState, { showBottomSheet = false })
 
     Scaffold(
         topBar = {
-            CustomTopAppBar(scrollBehavior,"Tareas")
+            CustomTopAppBar(scrollBehavior, "Tareas")
         },
         bottomBar = {
             CustomBottomAppBar(Navigation)
         },
         floatingActionButton = {
-            CustomFloatingActionButton(Navigation)
+            CustomFloatingActionButtonSecond(
+                onClick = {
+                    scope.launch {
+                        showBottomSheet = true
+                        bottomSheetState.show()
+                    }
+                }
+            )
         },
-        modifier = Modifier.nestedScroll((scrollBehavior.nestedScrollConnection))
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
-
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            TaskList(PaddingValues(0.dp))
+        Column(modifier = Modifier.padding(innerPadding)) {
+            TaskList(PaddingValues(0.dp), taskList)
         }
-
     }
 }
