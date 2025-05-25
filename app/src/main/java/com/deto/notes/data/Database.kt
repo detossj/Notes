4f1277d2a87f0cdd567053a0f406678259f32326
detossj <jorgerubilarssj@gmail.com>
2025-05-25 18:57:38 -0400

    Task added

--- app/src/main/java/com/deto/notes/data/Database.kt
+++ app/src/main/java/com/deto/notes/data/Database.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-@Database(entities = [Note::class], version = 1, exportSchema = false)
+@Database(entities = [Note::class, Task::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun noteDao(): NoteDao
 
