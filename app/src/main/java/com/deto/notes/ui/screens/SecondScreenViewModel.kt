af5c1c433ecf80dc8896137a3de73320f6afa95f
detossj <jorgerubilarssj@gmail.com>
2025-05-25 20:11:51 -0400

    functions added to SecondViewModel

--- app/src/main/java/com/deto/notes/ui/screens/SecondScreenViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.deto.notes.ui.screens
-
-import androidx.lifecycle.ViewModel
-import com.deto.notes.data.TaskRepository
-
-class SecondScreenViewModel(private val taskRepository: TaskRepository) : ViewModel() {
-
-    val taskList = taskRepository.getAllTasksStream()
-}
---
+++ app/src/main/java/com/deto/notes/ui/screens/SecondViewModel.kt
@@ -0,0 +1,63 @@
+package com.deto.tasks.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.deto.notes.data.Task
+import com.deto.notes.data.TaskRepository
+import kotlin.Int
+
+
+class SecondViewModel(private val taskRepository: TaskRepository) : ViewModel() {
+
+    val taskList = taskRepository.getAllTasksStream()
+
+    var newTaskUiState by mutableStateOf(NewTaskUIState())
+        private set
+
+    fun updateUiState(newTask: NewTask){
+        newTaskUiState =
+            NewTaskUIState(newTask = newTask, isEntryValid = validateInput(newTask))
+    }
+
+    suspend fun saveItem() {
+        if(validateInput()){
+            taskRepository.insertTask(newTaskUiState.newTask.toTask())
+        }
+    }
+
+    private fun validateInput(uiState: NewTask = newTaskUiState.newTask) : Boolean {
+        return with(uiState) {
+            title.isNotBlank()
+        }
+    }
+
+}
+data class NewTaskUIState(
+    val newTask: NewTask = NewTask(),
+    val isEntryValid: Boolean = false
+)
+
+data class NewTask(
+    val id: Int = 0,
+    val title: String = "",
+    val completed: Boolean = false
+)
+
+fun NewTask.toTask(): Task = Task(
+    id = id,
+    title = title,
+    completed = completed
+)
+
+fun Task.toItemUiState(isEntryValid: Boolean = false): NewTaskUIState = NewTaskUIState(
+    newTask = this.toItemDetails(),
+    isEntryValid = isEntryValid
+)
+
+fun Task.toItemDetails(): NewTask = NewTask(
+    id = id,
+    title = title,
+    completed = completed
+)
