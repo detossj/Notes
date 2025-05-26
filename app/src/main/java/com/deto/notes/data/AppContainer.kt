8f69a99204018704edcc70e7172b0f24513b49f1
detossj <jorgerubilarssj@gmail.com>
2025-05-25 20:09:20 -0400

    taskRepository added

--- app/src/main/java/com/deto/notes/data/AppContainer.kt
+++ app/src/main/java/com/deto/notes/data/AppContainer.kt
@@ -4,11 +4,16 @@ import android.content.Context
 
 interface AppContainer {
     val noteRepository: NoteRepository
+    val taskRepository: TaskRepository
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
     override val noteRepository: NoteRepository by lazy {
         LocalNoteRepository(AppDatabase.getDatabase(context).noteDao())
+    }
+
+    override val taskRepository: TaskRepository by lazy {
+        LocalTaskRepository(AppDatabase.getDatabase(context).taskDao())
     }
 
 }
