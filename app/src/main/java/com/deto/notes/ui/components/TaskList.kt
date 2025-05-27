6ac389304929f58b94a6925d3331065590bdf32a
detossj <jorgerubilarssj@gmail.com>
2025-05-27 14:23:54 -0400

    val and var added

--- app/src/main/java/com/deto/notes/ui/components/TaskList.kt
+++ app/src/main/java/com/deto/notes/ui/components/TaskList.kt
@@ -1,5 +1,6 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,11 +13,15 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,13 +29,25 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.deto.notes.data.Task
+import com.deto.notes.ui.AppViewModelProvider
+import com.deto.tasks.ui.screens.SecondViewModel
+import kotlinx.coroutines.launch
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskList(navController: NavController, innerPadding: PaddingValues, taskList: List<Task>, taskFilter: String) {
+fun TaskList(navController: NavController, innerPadding: PaddingValues, taskList: List<Task>, taskFilter: String,viewModel: SecondViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
+
+    val taskList by viewModel.taskList.collectAsState(initial = emptyList())
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+    //val task = taskList.find { it.id }
+
 
 
     Column(
@@ -47,12 +64,21 @@ fun TaskList(navController: NavController, innerPadding: PaddingValues, taskList
         ) {
             items(taskList) {
 
+
+
                 var isChecked by remember { mutableStateOf(it.completed) }
                 Card(
                     modifier = Modifier
                         .padding(5.dp)
+                        .clickable {
+                            scope.launch {
+                                showBottomSheet = true
+                                bottomSheetState.show()
+                            }
+
+                        }
                 ) {
-                    Row (
+                    Row(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(horizontal = 20.dp, vertical = 20.dp),
@@ -67,7 +93,7 @@ fun TaskList(navController: NavController, innerPadding: PaddingValues, taskList
                         )
 
                         Text(
-                            text = highlightMatch(it.title,taskFilter, Color.Yellow),
+                            text = highlightMatch(it.title, taskFilter, Color.Yellow),
                             modifier = Modifier.padding(start = 8.dp),
                             fontWeight = FontWeight.Bold
                         )
