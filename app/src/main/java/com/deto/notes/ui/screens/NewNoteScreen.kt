c39b8a35c7d5522913e635861b07a4c17c57ce85
detossj <jorgerubilarssj@gmail.com>
2025-05-30 20:07:36 -0400

    Text with date added

--- app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
+++ app/src/main/java/com/deto/notes/ui/screens/NewNoteScreen.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -16,6 +17,7 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.deto.notes.ui.AppViewModelProvider
@@ -106,12 +109,31 @@ fun NewNoteScreen(Navigation: NavController, noteId: Int? = null, viewModel: New
             ) {
 
                if(note == null) {
-                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },"Título",32)
+                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },"Título",24)
+
+                   Text(
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(16.dp),
+                       text = viewModel.newNoteUiState.newNote.date,
+                       fontSize = 14.sp,
+                       color = Color.Gray
+
+                   )
 
                    CustomOutlinedTextField(viewModel.newNoteUiState.newNote.content, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(content = it)) },"Empiece a escribir",16)
                }
                else {
-                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },note.title,32)
+                   CustomOutlinedTextField(viewModel.newNoteUiState.newNote.title, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(title = it)) },note.title,24)
+
+                   Text(
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(16.dp),
+                       text = viewModel.newNoteUiState.newNote.date,
+                       fontSize = 14.sp,
+                       color = Color.Gray
+                   )
 
                    CustomOutlinedTextField(viewModel.newNoteUiState.newNote.content, { viewModel.updateUiState(viewModel.newNoteUiState.newNote.copy(content = it)) },note.content,16)
                }
