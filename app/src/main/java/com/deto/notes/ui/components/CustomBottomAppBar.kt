9b58ad265028a1e84619d33d6c7cac33000f7077
detossj <jorgerubilarssj@gmail.com>
2025-05-31 22:39:15 -0400

    strings added to buttons

--- app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBar.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -63,7 +64,7 @@ fun CustomBottomAppBar(Navigation: NavController) {
                         tint = if(currentRoute == HomePage::class.qualifiedName){ Color.White } else Color.Gray
                     )
 
-                    Text("Notas")
+                    Text(stringResource(R.string.title_notes))
                 }
             }
 
@@ -89,7 +90,7 @@ fun CustomBottomAppBar(Navigation: NavController) {
                         tint = if(currentRoute == SecondPage::class.qualifiedName){ Color.White } else Color.Gray
                     )
 
-                    Text("Tareas")
+                    Text(stringResource(R.string.title_tasks))
                 }
             }
         }
