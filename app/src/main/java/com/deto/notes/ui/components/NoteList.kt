f208eb94fd5ccc8114abb7fda159a69b37b509a7
detossj <jorgerubilarssj@gmail.com>
2025-06-02 20:43:49 -0400

    function combinedClickable and Icons to Card added

--- app/src/main/java/com/deto/notes/ui/components/NoteList.kt
+++ app/src/main/java/com/deto/notes/ui/components/NoteList.kt
@@ -1,6 +1,7 @@
 package com.deto.notes.ui.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,16 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -30,10 +31,11 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.deto.notes.NotePage
 import com.deto.notes.data.Note
+import com.deto.notes.R
 
-
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NoteList(navController: NavController, innerPadding: PaddingValues, notes: List<Note>, notesFilter: String) {
+fun NoteList(navController: NavController, innerPadding: PaddingValues, notes: List<Note>, notesFilter: String, selected: Set<Int>, modeSelection: Boolean, onToggleSelection: (Int) -> Unit, onLongPress: (Int) -> Unit) {
 
 
     Column(
@@ -50,11 +52,22 @@ fun NoteList(navController: NavController, innerPadding: PaddingValues, notes: L
             contentPadding = PaddingValues(10.dp),
             modifier = Modifier.fillMaxWidth()
         ) {
-            items(notes) {
+            items(notes) { note ->
                 Card(
                     modifier = Modifier
                         .padding(5.dp)
-                        .clickable { navController.navigate(NotePage(it.id)) },
+                        .combinedClickable(
+                            onClick = {
+                                if (modeSelection) {
+                                    onToggleSelection(note.id)
+                                } else {
+                                    navController.navigate(NotePage(note.id))
+                                }
+                            },
+                            onLongClick = {
+                                onLongPress(note.id)
+                            }
+                        )
                 ) {
                     Column(
                         modifier = Modifier
@@ -64,16 +77,16 @@ fun NoteList(navController: NavController, innerPadding: PaddingValues, notes: L
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.Start
                     ) {
-                        val titlePreview = if (it.title.length > 13) {
-                            it.title.take(13) + "..."
+                        val titlePreview = if (note.title.length > 13) {
+                            note.title.take(13) + "..."
                         } else {
-                            it.title
+                            note.title
                         }
 
-                        val contentPreview = if (it.content.length > 60) {
-                            it.content.take(60) + "..."
+                        val contentPreview = if (note.content.length > 60) {
+                            note.content.take(60) + "..."
                         } else {
-                            it.content
+                            note.content
                         }
 
                         Text(
@@ -86,10 +99,28 @@ fun NoteList(navController: NavController, innerPadding: PaddingValues, notes: L
                             color = Color.Gray,
                         )
                         Text(
-                            text = it.date,
+                            text = note.date,
                             color = Color.Gray,
                             fontSize = 14.sp
                         )
+
+                        if (modeSelection) {
+                            if (selected.contains(note.id)) {
+                                Icon(
+                                    imageVector = Icons.Default.CheckCircle,
+                                    contentDescription = null,
+                                    tint = Color(0xFFFFC107),
+                                    modifier = Modifier.align(Alignment.End)
+                                )
+                            } else {
+                                Icon(
+                                    painter = painterResource(R.drawable.radio_button_unchecked_24dp_1f1f1f_fill0_wght400_grad0_opsz24),
+                                    contentDescription = null,
+                                    tint = Color.Gray,
+                                    modifier = Modifier.align(Alignment.End)
+                                )
+                            }
+                        }
                     }
                 }
             }
