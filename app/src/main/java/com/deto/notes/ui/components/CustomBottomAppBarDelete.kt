8235d71d54d5dbbea398f91431de2861b7df8e9d
detossj <jorgerubilarssj@gmail.com>
2025-06-05 15:31:53 -0400

    BottomAppBar added

--- app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBarDelete.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomBottomAppBarDelete.kt
@@ -1,2 +1,63 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.deto.notes.HomePage
+import com.deto.notes.R
+
+@Composable
+fun CustomBottomAppBarDelete(Navigation: NavController, onClick: () -> Unit) {
+    BottomAppBar(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(
+                onClick = onClick,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.Transparent,
+                    disabledContentColor = Color.White
+                )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Notas",
+                        modifier = Modifier.size(30.dp),
+                        tint = Color.White
+                    )
+
+                    Text(stringResource(R.string.customBottomAppBarDelete_text))
+                }
+            }
+
+        }
+    }
+}
