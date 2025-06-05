e88f9ecf8a7b2a4cb49fd300f337f85ecc935cae
detossj <jorgerubilarssj@gmail.com>
2025-06-05 09:49:16 -0400

    function deleteNoteById added

--- app/src/main/java/com/deto/notes/ui/screens/HomeViewModel.kt
+++ app/src/main/java/com/deto/notes/ui/screens/HomeViewModel.kt
@@ -1,11 +1,22 @@
 package com.deto.notes.ui.screens
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.deto.notes.data.NoteRepository
+import kotlinx.coroutines.launch
 
 
 class HomeViewModel (private val noteRepository: NoteRepository) : ViewModel() {
 
     val noteList = noteRepository.getAllNotesStream()
+
+    fun deleteNoteById(selected:  List<Int>) {
+        viewModelScope.launch {
+
+            for ( id in selected ) {
+                noteRepository.deleteNoteById(id)
+            }
+        }
+    }
 
 }
