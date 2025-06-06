e9710018434c6bf74677931990b6e067a9590a49
detossj <jorgerubilarssj@gmail.com>
2025-06-06 16:01:46 -0400

    deleteTask changed to deleteTaskById

--- app/src/main/java/com/deto/notes/data/LocalTaskRepository.kt
+++ app/src/main/java/com/deto/notes/data/LocalTaskRepository.kt
@@ -7,7 +7,7 @@ class LocalTaskRepository(private val itemDao: TaskDao) : TaskRepository {
 
     override suspend fun updateTask(task: Task) = itemDao.update(task)
 
-    override suspend fun deleteTask(task: Task) = itemDao.delete(task)
+    override suspend fun deleteTaskById(id: Int) = itemDao.deleteById(id)
 
     override fun getAllTasksStream(): Flow<List<Task>> = itemDao.getAllItems()
 
