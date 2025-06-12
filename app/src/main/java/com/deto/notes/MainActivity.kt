f3e0d70431a71ce7c5c25cc713d3dd98cffed90d
detossj <jorgerubilarssj@gmail.com>
2025-06-12 18:20:32 -0400

    theme

--- app/src/main/java/com/deto/notes/MainActivity.kt
+++ app/src/main/java/com/deto/notes/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.deto.notes.ui.theme.NotesTheme
+import com.deto.notes.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NotesTheme {
+            AppTheme {
                 Navigation()
             }
         }
