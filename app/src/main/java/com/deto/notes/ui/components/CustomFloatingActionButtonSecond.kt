d7a64d66dd491f9d6dc75ac4f36392054df3228c
detossj <jorgerubilarssj@gmail.com>
2025-06-12 18:25:43 -0400

    shape and size added

--- app/src/main/java/com/deto/notes/ui/components/CustomFloatingActionButtonSecond.kt
+++ app/src/main/java/com/deto/notes/ui/components/CustomFloatingActionButtonSecond.kt
@@ -1,10 +1,14 @@
 package com.deto.notes.ui.components
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.deto.notes.ui.theme.onPrimaryDark
 import com.deto.notes.ui.theme.onPrimaryLight
 
@@ -14,8 +18,13 @@ fun CustomFloatingActionButtonSecond(onClick: () -> Unit) {
     FloatingActionButton(
         onClick = { onClick() },
         containerColor = onPrimaryLight,
-        contentColor = onPrimaryDark
+        contentColor = onPrimaryDark,
+        shape = RoundedCornerShape(30.dp)
     ) {
-        Icon(Icons.Default.Add, contentDescription = "Agregar Tarea")
+        Icon(
+            Icons.Default.Add,
+            contentDescription = "Agregar Tarea",
+            modifier = Modifier.size(32.dp)
+        )
     }
 }
